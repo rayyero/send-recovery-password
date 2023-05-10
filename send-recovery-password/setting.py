06d7cf8b8182d0de3c9d6beddc4a5d2934803555
rayyero <raydelyero96@gmail.com>
2023-05-10 14:35:45 -0400

    Initial commit

---
+++ send-recovery-password/setting.py
@@ -0,0 +1,13 @@
+
+#variables para config y BDconection
+
+MAIL_USERNAME="raydelyero96@gmail.com"
+MAIL_PASSWORD="sprfriknyvuudfte"
+MAIL_FROM="raydelyero96@gmail.com"
+MAIL_PORT=587
+MAIL_SERVER="smtp.gmail.com"
+DB_HOST="lin-13704-4133-pgsql-primary.servers.linodedb.net"
+DB_NAME="aba_test"
+DB_USER="linpostgres"
+DB_PASSWORD="HxywGpAs2-2CnbGh"
+URL_COMPANY="https://test.createuser.abaanalystgroup.live"
